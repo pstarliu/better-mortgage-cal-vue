21d99685318f98c2af57f940be0779261fb93425
Paul Liu <pstar.liu@gmail.com>
2018-10-21 17:10:36 +1300

    Update website icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,10 @@
 
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
+    <!-- <b-navbar-brand href="#"><img src="../assets/logo.png" style="height:32px;" alt="house"></b-navbar-brand> -->
+    <b-navbar-brand to="/">
+      <img src="../assets/logo.png" style="height:32px;" alt="house">
+    </b-navbar-brand>
     <b-collapse is-nav id="nav_collapse">
 
       <b-navbar-nav>
