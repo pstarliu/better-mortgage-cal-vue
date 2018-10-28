871ad98a835cdd01c31acc0c333c66a6fb4c60ac
Paul Liu <pstar.liu@gmail.com>
2018-10-28 16:55:02 +1300

    Change indending.
    Atom is rather usesless for editiong Vuejs file, I give up and use
    Emacs instead, seems working better so far.

--- src/components/Calculate.vue
+++ src/components/Calculate.vue
@@ -1,51 +1,53 @@
 <template>
-  <div>
-    <h1>Doing the Calculate</h1>
-    <b-form>
-
-      <b-form-group id="bankSelectionGrp"
-      label="Your Bank"
-      label-for="bankSelection"
-      description="Who provide your loan">
-      <b-form-select id="bankSelection"
-      v-model="bankSelection"
-      :options="bankOptions" />
+<div>
+  <h1>Doing the Calculate</h1>
+  
+  <b-form>
+    
+    <b-form-group id="bankSelectionGrp" 
+		  label="Your Bank"
+		  label-for="bankSelection"
+		  description="Who provide your loan">
+      <b-form-select id="bankSelection" v-model="bankSelection" :options="bankOptions" />
     </b-form-group>
-
+    
     <b-form-group id="interestGrp"
-                        label="Interest Rate:"
-                        label-for="txbInterest">
-            <b-form-input id="txbInterest"
-                          type="percentage"
-                          v-model="interest"
-                          required
-                          placeholder="0%">
-            </b-form-input>
-          </b-form-group>
-
+		  label="Interest Rate:"
+		  label-for="txbInterest">
+      <b-form-input id="txbInterest"
+		    type="percentage"
+		    v-model="interest"
+		    required
+		    placeholder="0%">
+      </b-form-input>
+    </b-form-group>
+    
     <b-form-group id="loanAmountGrp"
-                    label="Total Loan Amount:"
-                    label-for="txbloanAmount">
-        <b-form-input id="txbLoanAmount"
-                      type="money"
-                      v-model="loanAmount"
-                      required
-                      placeholder="Loan Amount">
-        </b-form-input>
-      </b-form-group>
-
-      <b-form-group id="loanTermGrp"
-                    label="Loan Term:"
-                    label-for="txbLoanTerm">
-        <b-form-input id="txbLoanTerm"
-                      type="Number"
-                      v-model="loanTerm"
-                      required
-                      placeholder="The term for the loan">
-        </b-form-input>
-      </b-form-group>
+		  label="Total Loan Amount:"
+		  label-for="txbloanAmount">
+      <b-form-input id="txbLoanAmount"
+		    type="money"
+		    v-model="loanAmount"
+		    required
+		    placeholder="Loan Amount">
+      </b-form-input>
+    </b-form-group>
+    
+    <b-form-group id="loanTermGrp"
+		  label="Loan Term:"
+		  label-for="txbLoanTerm">
+      <b-form-input id="txbLoanTerm"
+		    type="Number"
+		    v-model="loanTerm"
+		    required
+		    placeholder="The term for the loan">
+      </b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
   </b-form>
-  </div>
+</div>
+
 </template>
 
 <script>
