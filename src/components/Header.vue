e6ae751c5928c23094707daf05dde417f39d5ee8
Paul Liu <pstar.liu@gmail.com>
2018-11-11 17:51:10 +1300

    Add Google authentication

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,94 @@
 <template>
-  <b-navbar toggleable="md" type="light" variant="faded">
+<b-navbar toggleable="md" type="light" variant="faded">
+  
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+  
+  <b-navbar-brand to="/">
+    <img src="../assets/logo.png" style="height:35px;" alt="house">
+  </b-navbar-brand>
+  <b-collapse is-nav id="nav_collapse">
+    
+    <b-navbar-nav>
+      <b-nav-item to="/calculate">Calculate</b-nav-item>
+      <b-nav-item to="/about">About</b-nav-item>
+    </b-navbar-nav>
+    
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      
+      <b-nav-item-dropdown text="Lang" right disabled>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ZH</b-dropdown-item>
+      </b-nav-item-dropdown>
 
-    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-    <b-navbar-brand to="/">
-      <img src="../assets/logo.png" style="height:35px;" alt="house">
-    </b-navbar-brand>
-    <b-collapse is-nav id="nav_collapse">
-
-      <b-navbar-nav>
-        <b-nav-item to="/calculate">Calculate</b-nav-item>
-        <b-nav-item to="/about">About</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-
-        <b-nav-item-dropdown text="Lang" right disabled>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ZH</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right disabled>
-          <!-- Using button-content slot -->
-          <template slot="button-content">
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Signout</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-
-    </b-collapse>
-  </b-navbar>
-
+      <b-nav-item href="#" @click="authorGoogle">SignIn</b-nav-item>
+      
+      <b-nav-item-dropdown right>
+	<!-- <button type="button">{{ userName }}</button> -->
+        <!-- Using button-content slot -->
+        <template slot="button-content">
+          <em v-model="userName">{{userName}}</em>
+        </template>
+        <b-dropdown-item href="#" v-if="isAuthorized">Profile</b-dropdown-item>
+        <b-dropdown-item href="#" v-if="true" @click="signOut">Signout</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+    
+  </b-collapse>
+</b-navbar>
 </template>
+
+<script>
+export default {
+    data() {
+	return {
+	    isAuthorized: false,
+	    userName: 'Not Sign In'
+	}
+    },
+    methods: {
+	authorGoogle() {
+	    var provider = new firebase.auth.GoogleAuthProvider();
+	    firebase.auth().signInWithPopup(provider).then( (result) => {
+		// This gives you a Google Access Token. You can use it to access the Google API.
+		var token = result.credential.accessToken;
+		var user = result.user;
+		// The signed-in user info.
+		this.userName = user.displayName;
+		let tempUserName = user.displayName;
+		this.isAuthorized = true;
+	    }).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// The email of the user's account used.
+		var email = error.email;
+		// The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential;
+		console.log("Sign in error, ", errorCode, errorMessage);
+	    });
+	},
+	signOut() {
+	    firebase.auth().signOut().then( () => {
+		// Sign-out successful.
+		this.isAuthorized = false;
+		this.userName = 'Not Sign In';
+		console.log("sign out successfull");
+	    }).catch(function(error) {
+		// An error happened.
+		this.isAuthorized = false;
+		this.userName = 'Not Sign In';
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// The email of the user's account used.
+		var email = error.email;
+		// The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential;
+		// ...
+		console.log("Error: " + errorCode + ", " + errorMessage);
+	    });
+	}	
+    }
+}
+</script>
