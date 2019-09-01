503b489e2523ac9604edef68c08f20d2f40e6b39
Paul Liu <pstar.liu@gmail.com>
2019-09-01 17:38:37 +1200

    Add term of service ifile

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Home from './components/Home.vue';
 import Calculate from './components/Calculate.vue';
 import About from './components/About.vue';
 import PrivacyPolicy from './components/PrivacyPolicy.vue';
+import TermOfService from './components/TermOfService.vue';
 import SignupPage from './components/auth/signup.vue';
 import SigninPage from './components/auth/signin.vue';
 
@@ -10,6 +11,7 @@ export const routes = [
   { path: '/calculate', component: Calculate },
   { path: '/about', component: About },
   { path: '/privacy', component: PrivacyPolicy },
+  { path: '/termofservice', component: TermOfService },
   { path: '/signup', component: SignupPage },
   { path: '/signin', component: SigninPage },
 ]
